57db79c0eaa93eee2d61f2e798b130b467b07aea
Kyndt Clovis <clovis@1000mercis.com>
2017-07-13 17:22:04 +0200

    add flag to parse arg

--- api/main.go
+++ api/main.go
@@ -1,14 +1,33 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+	var (
+		port   = flag.String("port", os.Getenv("EP17_API_PORT"), "Port to listen to")
+		dbName = flag.String("dbname", os.Getenv("APP_DB_NAME"), "Port to listen to")
+	)
+
+	// Set the addr with the port
+	var bindingPort int
+	bindingPort, err := strconv.Atoi(*port)
+	if err != nil {
+		bindingPort = 8888
+	}
+	addr := fmt.Sprintf(":%d", bindingPort)
+
+	// Setup & run application
 	a := App{}
 	a.Initialize(
 		os.Getenv("APP_DB_ADDRESS"),
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		os.Getenv(*dbName))
 
-	a.Run(":8888")
+	a.Run(addr)
 }
